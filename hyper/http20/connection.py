524fc9309ebfe2e62750244435b4f9458407acec
Norman Moeschter-Schenck <norman.moeschter-schenck@viz.ai>
2022-04-29 17:07:32 +0100

    Adapt to h2>=4 restructuring.

--- hyper/http20/connection.py
+++ hyper/http20/connection.py
@@ -7,7 +7,7 @@ Objects that build hyper's connection-level HTTP/2 abstraction.
 """
 import h2.connection
 import h2.events
-import h2.settings
+from h2.settings import SettingCodes
 
 from ..compat import ssl
 from ..tls import wrap_socket, H2_NPN_PROTOCOLS, H2C_PROTOCOL
@@ -387,7 +387,7 @@ class HTTP20Connection(object):
         with self._conn as conn:
             conn.initiate_upgrade_connection()
             conn.update_settings(
-                {h2.settings.ENABLE_PUSH: int(self._enable_push)}
+                {SettingCodes.ENABLE_PUSH: int(self._enable_push)}
             )
         self._send_outstanding_data()
 
@@ -408,7 +408,7 @@ class HTTP20Connection(object):
         with self._conn as conn:
             conn.initiate_connection()
             conn.update_settings(
-                {h2.settings.ENABLE_PUSH: int(self._enable_push)}
+                {SettingCodes.ENABLE_PUSH: int(self._enable_push)}
             )
         self._send_outstanding_data()
 
